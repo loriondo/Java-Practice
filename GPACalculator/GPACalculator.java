1b35bde78dfbdde27544726440ad08966456dcab
Lou Oriondo <loriondo@uga.edu>
2014-08-21 21:21:51 -0400

    Functionality Change
    The university Degreeworks website does not provide a PDF of a
    student’s courses and grades. Instead, it opens up a pop-up window
    which can be saved as a XML file - saving the file does not work (seems
    to be security related).
    
    Converted PDF option to quick insert option, where GPA is calculated
    after every entry and course name is ignored.

--- GPACalculator/GPACalculator.java
+++ GPACalculator/GPACalculator.java
@@ -36,18 +36,56 @@ public class GPACalculator {
        Scanner scanner = new Scanner(System.in);
        
        int options;
-       out.println("Press 1 to read in a file with your grades.");
-       out.println("Otherwise, press 2 to input information manually.");
-       if((options = scanner.nextInt()) == 1){
-           out.println("Please select your file.");
-           out.println("Not yet implemented. Goodbye.");
-           System.exit(0);
-       }else{
+       out.println("Press 1 if you only care about numbers.");
+       out.println("Press 2 for to include your courses and save the results.");
+       out.println("Press any other key to exit the program.");
+       options = scanner.nextInt();
+       if(options == 1){
+          String input;
+          boolean complete = false;
+          
+          out.println("-----------------------------");
+          out.println("You have chosen to calculate GPA the quick way.");
+          out.println("Enter your letter grade and course credit hours like this: ");
+          out.println("'A- 4 OR a- 4'" + "\n--------------------------------");
+          out.println("Your weighted GPA will be displayed as you enter grades.");
+          out.println("Type delete to remove the previous entry.");
+          out.println("Type exit to terminate the program.");
+          
+          while(!complete){
+              out.print(">> ");
+              input = scanner.nextLine();
+              if(input.toLowerCase().equals("exit")){
+                  out.println("Terminating program.");
+                  complete = true;
+              }else if(input.toLowerCase().equals("delete")){
+                  if(!courseGrades.isEmpty()){
+                    out.println("Removing previous grade entry.");
+                    courseGrades.remove(courseGrades.size()-1);
+                  }else{
+                      out.println("Can't delete from an empty list.");
+                  }
+              }else if(!input.isEmpty()){
+                  String temp[] = input.split(" ", 2);
+                  if(temp.length != 2){
+                      out.println("Improper input. Format must be: grade hours");
+                  }else if(temp.length == 2){
+                      int hours = Integer.valueOf(temp[1]);
+                      Grade grades = new Grade(null, temp[0], hours);
+                      courseGrades.add(grades);
+                      
+                      //calculate gpa every entry?
+                      //give option to save to document when exiting?
+                  }
+              }
+          }
+           
+       }else if(options  == 2){
            out.println("Enter information manually.");
            String input;
            boolean complete = false;
            out.println("Just follow the instructions to input your grades.");
-           out.println("Whenever you're done, just type in 'calculate'.");
+           out.println("Whenever you're done, just type 'calculate' and hit enter.");
 
            while(!complete){
                out.println("First, type in the course name. Then hit enter.");
@@ -80,7 +118,10 @@ public class GPACalculator {
            out.println("Now calculating your weighted GPA.");
            out.println("Weighted GPA: " + calc.calculateGPA(courseGrades));
 
-        }
+        }else{
+           out.println("Exiting program.");
+           System.exit(0); 
+       }
     }
     
 }
