993aa43085208172cadee08998b6cc5e11b5ef78
Lou Oriondo <loriondo@uga.edu>
2014-11-03 18:03:12 -0500

    More Jsoup tinkering.
    HTML document = a whole bunch of elements
    
    Now working on ways to reduce elements into parts to work with.

--- Playground/Playground.java
+++ Playground/Playground.java
@@ -7,6 +7,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.select.Elements;
 
 /**
  * Playground.java.
@@ -33,11 +36,13 @@ public class Playground {
         solve2.passBy(solve2);
         out.println(solve2.test);*/
         
-        String testURL1 = "http://www.oracle.com/technetwork/articles/java/index-137868.html";
-        String testURL2 = "http://us.battle.net/d3/en/class/crusader/";
-        ArrayList<String> text = solve
-                .getTextFromURL(testURL1);
+        String testURL1 = "http://diablo.somepage.com/popular/crusader#builds";
+        //Goal is to get all the information I need from the diablo game guide
+        //and various class build sites to make my own 'database' of information.
+        //Except it wouldnt be a database, the information would have to accessed
+        //everytime the program runs
         
+        /*ArrayList<String> text = solve.getTextFromURL(testURL1);
         text
                 .stream()
                 .forEach(t -> {
@@ -46,6 +51,27 @@ public class Playground {
                     }
                     out.print(t + " "); //this is still pretty messy
                 });
+        */
+        
+        /*ArrayList<Element> e = new ArrayList(solve.getElementsFromURL(testURL1));
+        out.println(e.size()); //this is way too big, I don't want all the elements
+        */
+        
+        ArrayList<Element> content = new ArrayList(solve.getElementByTag(testURL1, "body"));
+        out.println(content.size()); //only 1 element with this tag, good
+        //out.println(content.get(0).toString()); //but how do we filter an element?
+          
+        Element body = content.get(0);  //break this down even further...
+        //out.println(body.toString()); 
+        
+        ArrayList<TextNode> tn = new ArrayList(body.textNodes());
+        out.println(tn.size());
+        
+        for(TextNode text: tn){
+            out.println("/* --- Text Node --- */");
+            out.println(text.isBlank()); //text nodes are blank, whhyyy
+            out.println("/* --- Text Node --- */");
+        }
         
     }//main
     
@@ -110,7 +136,6 @@ public class Playground {
          * the text from the URL. This can be very long and cannot be read. To 
          * aid in readability, this retrieves the text from the URL and splits
          * the text by spaces. 
-         * 
          * @param   url URL of the web page text will be retrieved
          * @return      List all words separated by a space
          * @see         Jsoup
@@ -133,6 +158,56 @@ public class Playground {
             ArrayList<String> text = new ArrayList<>(Arrays.asList(temp));
             return text;
         }//getText
+        
+        /**
+         * Retrieves all elements that make up the HTML document found in the
+         * provided url. Elements start with a 'start' tag, end with an 'end' 
+         * tag, and have content in between. The getAllElements will end up 
+         * retrieving ALL of this, which doesn't seem to be of much help. 
+         * End goal is to retrieve only the text content within a certain
+         * element. 
+         * @param   url URL of web page to be accessed 
+         * @return      List of type Element, all elements in the HTML document 
+         */
+        public Elements getElementsFromURL(String url){
+            Document doc = null;
+            try {
+                out.println("Attempting to connect to " + url + ".");
+                doc = Jsoup
+                        .connect(url)
+                        .get();
+            }catch(IOException e){
+                out.println("Error connecting to URL: " + e.getMessage());
+            }finally {
+                out.println("Attempt to connect to URL completed.");
+            }
+            return doc.getAllElements();
+        }//getElements
+        
+        /**
+         * Retrieves all elements from the HTML document that have the 
+         * specified tag. For example, the 'body' tag will hold content 
+         * (that I want access to). As such, searching for this tag decreases
+         * the number of elements that I have to filter through.
+         * @param url   URL of web page to be accessed
+         * @param tag   tag of specific element, i.e. body
+         * @return      list of type Element that has elements with specified tag
+         */
+        public Elements getElementByTag(String url, String tag){
+            Document doc = null;
+            try {
+                out.println("Attempting to connect to " + url + ".");
+                doc = Jsoup
+                        .connect(url)
+                        .get();
+            }catch(IOException e){
+                out.println("Error connecting to URL: " + e.getMessage());
+            }finally {
+                out.println("Attempt to connect to URL completed.");
+            }
+            return doc.getElementsByTag(tag);
+        }//getElementsByTag
+        
         
         
     }//Practice class
